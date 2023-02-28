3d50fd943f5192abc22cc0f98d6522697f4c959a
ahmedyh222 <85297340+ahmedyh222@users.noreply.github.com>
2023-03-01 00:16:32 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -22,5 +22,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('/chkuname/{username}', [authController::class, 'check_user']);
 Route::post('/adduser', [authController::class, 'adduser']);
 Route::post('/login', [authController::class, 'login']);
+Route::get('/users/{id}', 'UserController@getUserData');
 
 /*Route::post('/logout', [AuthController::class, 'logout']);*/
