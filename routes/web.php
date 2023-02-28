1358e1e225c2dbe3fb7f7dba483d7af0a8ea9eac
ahmedyh222 <85297340+ahmedyh222@users.noreply.github.com>
2023-03-01 00:26:56 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -32,4 +32,4 @@ Route::middleware([
 Route::get('/chkuname/{username}', [authController::class, 'check_user']);
 Route::post('/adduser', [authController::class, 'adduser']);
 Route::post('/login', [authController::class, 'login']);
-
+Route::get('/user/{id}',[userController::class,'getUserData']);
