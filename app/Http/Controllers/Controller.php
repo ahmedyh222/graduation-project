2aa22abe07707464096e860ffdd3de6ecf2a1293
ahmedyh222 <85297340+ahmedyh222@users.noreply.github.com>
2023-03-01 00:28:51 +0200

    Update Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,37 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+     public function getUserData($user_id)
+    {
+        $user = User::where('id', $user_id)->first();
+
+        $userData = [
+            'state' => 'good, ok',
+            'message' => 'information retreived successfully',
+            'data' => [
+                'user' => [
+                    'user_id' => $user->id,
+                    'nick_name' => $user->nick_name,
+                    'user_type' => $user->user_type,
+                    'bdate' => $user->bdate,
+                    'gender' => $user->gender,
+                    'prefix' => $user->prefix,
+                    'pnumber' => $user->pnumber,
+                    'email' => $user->email,
+                    'province' => $user->province,
+                    'city' => $user->city,
+                    'street' => $user->street,
+                    'img_url' => $user->img_url,
+                    'age' => $user->age,
+                    'img_urls' => [
+                        [
+                            'img_url' => $user->img_url
+                        ]
+                    ]
+                ]
+            ]
+        ];
+
+        return response()->json($userData);
+    }
 }
