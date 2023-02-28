6d1f03e90aa32bda4d586e16a08218ee4dab9699
Ahmed Essam <ahmed.essameldin262@gmail.com>
2023-02-28 22:36:15 +0200

    after updating authController

--- routes/api.php
+++ routes/api.php
@@ -20,12 +20,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::get('/chkuname/{username}', [authController::class, 'check_user']);
-<<<<<<< HEAD
 Route::post('/adduser', [authController::class, 'adduser']);
 Route::post('/login', [authController::class, 'login']);
 
-=======
-Route::post('/req/adduser', [authController::class, 'adduser']);
-Route::post('/req/login', [authController::class, 'login']);
->>>>>>> b5a5da57e5ac4fa9b99f66160d90928e1041d966
 /*Route::post('/logout', [AuthController::class, 'logout']);*/
