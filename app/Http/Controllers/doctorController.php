1266aee81647340a8fcc957f1d607946f5dffd75
ahmedyh222 <85297340+ahmedyh222@users.noreply.github.com>
2023-03-01 01:41:00 +0200

    Update doctorController.php

--- app/Http/Controllers/doctorController.php
+++ app/Http/Controllers/doctorController.php
@@ -6,5 +6,32 @@ use Illuminate\Http\Request;
 
 class doctorController extends Controller
 {
-    //
+    
+    public function getDoctorData($user_id)
+    {
+        $doctor = Doctor::where('id', $user_id)->first();
+
+        $doctorData = [
+            'state' => 'good, ok',
+            'message' => 'information retreived successfully',
+            'data' => [
+                'user' => [
+                    'doctor_id' => $doctor->id,
+                    'user_name' => $doctor->username,
+                    'rate'=>$doctor->rate,
+                    'specialty'=>$doctor->specialty,
+                    'nick_name'=>$doctor->name,
+                    'fees'=>$doctor->salary,
+                    'about'=>$doctor->about,
+                    'img_urls' => [
+                        [
+                            'img_url' => $doctor->img_url
+                        ]
+                    ]
+                ]
+            ]
+        ];
+
+        return response()->json($doctorData);
+    }
 }
