e78c6d36d597193e8b75dd2a193812c87ee83d77
ahmedyh222 <85297340+ahmedyh222@users.noreply.github.com>
2023-03-02 16:12:25 +0200

    Update userController.php

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -7,5 +7,37 @@ use Illuminate\Http\Request;
 
 class userController extends Controller
 {
+  public function getUserData($user_id)
+    {
+        $user = User::where('id', $user_id)->first();
 
+        $userData = [
+            'state' => 'good, ok',
+            'message' => 'information retreived successfully',
+            'data' => [
+                'user' => [
+                    'user_id' => $user->id,
+                    'nick_name' => $user->name,
+                    'user_type' => $user->user_type,
+                    'bdate' => $user->bdate,
+                    'gender' => $user->gender,
+                    'prefix' => $user->prefix,
+                    'pnumber' => $user->pnumber,
+                    'email' => $user->email,
+                    'province' => $user->province,
+                    'city' => $user->city,
+                    'street' => $user->street,
+                    'img_url' => $user->img_url,
+                    'age' => $user->age,
+                    'img_urls' => [
+                        [
+                            'img_url' => $user->img_url
+                        ]
+                    ]
+                ]
+            ]
+        ];
+
+        return response()->json($userData);
+    }
 }
