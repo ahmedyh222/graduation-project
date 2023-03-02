6d50a698505545a5584800affda8995539bad697
Ahmed Magdy <85253814+Ahmed5920@users.noreply.github.com>
2023-03-02 13:41:59 +0200

    Update feedbackController.php

--- app/Http/Controllers/feedbackController.php
+++ app/Http/Controllers/feedbackController.php
@@ -2,9 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use App\Models\Doctor;
+use App\Models\Staff;
+use App\Models\Feedback;
 use Illuminate\Http\Request;
 
 class feedbackController extends Controller
 {
-    //
+    public function filter_feedback($username){
+
+
+        $userdoc = User::where('username',$username)->first();
+
+        $doctor =  Doctor::where('username',$username)->first();
+
+        $feedback = feedback::where('feedback_to', $doctor->id)->get();
+        $res=array();
+        foreach ($feedback as $f) {
+            $userpat = User::where('id', $f->feedback_from)->first();
+            $userData = [
+                'state' => 'good, ok',
+                'message' => 'information retreived successfully',
+                'data' => [
+                    'user' => [
+                        'feedback_from' => $f->feedback_from,
+                        'feedback_to' => $f->feedback_to,
+                        'rate' => $f->rate,
+                        'issued_time' => $f->updated_at,
+                        'feedback' => $f->feedback,
+                        'uimgUrl' => $userpat->img_url,
+                        'dimgUrl' => $userdoc->img_url,
+                        'username' => $userpat->username,
+                        'doctorName' => $doctor->name,
+                    ]
+                ]
+            ];
+            array_push($res,$userData);
+
+        }
+        return response(compact('res'),200);
+
+    }
 }
